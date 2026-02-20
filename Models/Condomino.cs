42ea40ad599e5618d9d0feb1d9625582c5676820
ztban11-a181y <ztban11@gmail.com>
2026-02-19 20:30:24 -0600

    Avances 19_feb

---
+++ Models/Condomino.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CondominosAppWeb.Models
+{
+    public class Condomino
+    {
+        public string TipoID { get; set; }
+        public string ID { get; set; }
+        public string Nombre { get; set; }
+        public string Apellidos { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string NumeroFilial { get; set; }
+        public bool tieneConstruccion { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Registro.aspx.cs
@@ -0,0 +1,32 @@
+﻿using CondominosAppWeb.Models;
+using CondominosAppWeb.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CondominosAppWeb
+{
+    public partial class Registro : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        [WebMethod]
+        public static string RegistrarCondomino(Condomino elCondomino)
+        {
+            if (CondominoService.EmailExists(elCondomino.Email))
+            {
+                return "Email ya se encuentra registrado.";
+            }
+
+            CondominoService.AddOwner(elCondomino);
+            return "Registro satisfactorio!";
+        }
+    }
+}
